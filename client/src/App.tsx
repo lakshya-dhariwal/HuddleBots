8869c1e373b4682760d3313e25d42cb6f6909d27
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2023-12-09 22:20:56 +0530

    add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Route } from "wouter";
+import toast, { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
- 
+      <Toaster />
+      <div className="">
+        <Route path="/">
+          <Home />
+        </Route>
+      </div>
     </div>
   );
 }
