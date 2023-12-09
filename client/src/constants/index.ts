614f390205e3ae799677bfc068dbad70bc5bf80d
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2023-12-10 01:47:58 +0530

    contants

---
+++ client/src/constants/index.ts
@@ -0,0 +1,44 @@
+export const BACKEND = process.env.REACT_APP_BACKEND as string;
+
+export const AGENTS = [
+  {
+    name: "Snoopstein",
+    description:
+      "The intelligence of Albert Einstein in the swagger of Snoop Dog. Answers all your queries in the style of Rap and Rhyme",
+    voiceId: "",
+    image:
+      "https://res.cloudinary.com/apideck/image/upload/w_196,f_auto/v1679537948/marketplaces/clf3re5bf5163076zyg7byd2oqm/listings/CrirFriN_400x400_swcqof.jpg",
+  },
+  {
+    name: "Snoopstein",
+    description:
+      "The intelligence of Albert Einstein in the swagger of Snoop Dog. Answers all your queries in the style of Rap and Rhyme",
+    voiceId: "",
+    image:
+      "https://res.cloudinary.com/apideck/image/upload/w_196,f_auto/v1679537948/marketplaces/clf3re5bf5163076zyg7byd2oqm/listings/CrirFriN_400x400_swcqof.jpg",
+  },
+  {
+    name: "Snoopstein",
+    description:
+      "The intelligence of Albert Einstein in the swagger of Snoop Dog. Answers all your queries in the style of Rap and Rhyme",
+    voiceId: "",
+    image:
+      "https://res.cloudinary.com/apideck/image/upload/w_196,f_auto/v1679537948/marketplaces/clf3re5bf5163076zyg7byd2oqm/listings/CrirFriN_400x400_swcqof.jpg",
+  },
+  {
+    name: "Snoopstein",
+    description:
+      "The intelligence of Albert Einstein in the swagger of Snoop Dog. Answers all your queries in the style of Rap and Rhyme",
+    voiceId: "",
+    image:
+      "https://res.cloudinary.com/apideck/image/upload/w_196,f_auto/v1679537948/marketplaces/clf3re5bf5163076zyg7byd2oqm/listings/CrirFriN_400x400_swcqof.jpg",
+  },
+  {
+    name: "Snoopstein",
+    description:
+      "The intelligence of Albert Einstein in the swagger of Snoop Dog. Answers all your queries in the style of Rap and Rhyme",
+    voiceId: "",
+    image:
+      "https://res.cloudinary.com/apideck/image/upload/w_196,f_auto/v1679537948/marketplaces/clf3re5bf5163076zyg7byd2oqm/listings/CrirFriN_400x400_swcqof.jpg",
+  },
+];
